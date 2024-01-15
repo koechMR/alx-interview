3d59c1d39970e6a7e92c3975b96d46754f622808
koechMR <briankipkoech328@gmail.com>
2024-01-15 13:07:02 +0300

    minimum operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+""" Minimum Operations """
+
+
+def minOperations(n):
+    """ def operation """
+    if not isinstance(n, int):
+        return 0
+
+    operations = 0
+    iterator = 2
+    while (iterator <= n):
+        if not (n % iterator):
+            n = int(n / iterator)
+            operations += iterator
+            iterator = 1
+        iterator += 1
+    return operations

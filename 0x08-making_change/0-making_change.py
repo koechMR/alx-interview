d4a7b16113843f3e178322e10e63aa0a91356d42
koechMR <briankipkoech328@gmail.com>
2024-02-28 07:56:17 +0300

    new

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,17 +1,27 @@
 #!/usr/bin/python3
-"""
-Change comes from within
+"""Change comes from within
 """
 
 
 def makeChange(coins, total):
-    """
-    pile of coins of different values
+    """Return fewest number of coins needed to meet.
     """
     if total <= 0:
         return 0
-    dp = [0] + [float("inf")] * (total)
-    for coin in coins:
-        for x in range(coin, total + 1):
-            dp[x] = min(dp[x], dp[x - coin] + 1)
-    return dp[-1] if dp[-1] != float("inf") else -1
+
+    # Coins validity checks if the coins is None or empty
+    if (coins is None or len(coins) == 0):
+        return -1
+
+    change = 0
+    available_coins = sorted(coins, reverse=True)
+    change_left = total
+
+    for coin in available_coins:
+        while (change_left % coin >= 0 and change_left >= coin):
+            change += int(change_left / coin)
+            change_left = change_left % coin
+
+    change = change if change_left == 0 else -1
+
+    return change

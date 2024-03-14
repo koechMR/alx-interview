895912b7769cc501b03c5fd00bad95786652eebf
koechMR <briankipkoech328@gmail.com>
2024-03-14 08:10:53 +0300

    new

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,51 +1,28 @@
 #!/usr/bin/python3
-""" Prime Game"""
+"""Prime game module.
+"""
 
 
-def isprime(n):
-    """prime number """
-    for i in range(2, n):
-        if n % i == 0:
-            return False
-    return True
-
-
-def delete_numbers(n, nums):
-    """return zero """
-    for i in range(len(nums)):
-        if nums[i] % n == 0:
-            nums[i] = 0
-
-
-def isWinner(x, nums):
-    """ Return name of player
+def isWinner(v, nums):
+    """Determines the winner of the game.
     """
-    nums.sort()
-    winner = False
-    Maria = 0
-    Ben = 0
-    for game in range(x):
-        nums2 = list(range(1, nums[game] + 1))
-        turn = 0
-        while True:
-            """
-            monitor turns, uncomment to watch
-            """
-            change = False
-            for i, n in enumerate(nums2):
-                if n > 1 and isprime(n):
-                    delete_numbers(n, nums2)
-                    change = True
-                    turn += 1
-                    break
-            if change is False:
-                break
-        if turn % 2 != 0:
-            Maria += 1
-        else:
-            Ben += 1
-    if Maria == Ben:
+    if v < 1 or not nums:
         return None
-    if Maria > Ben:
-        return "Maria"
-    return "Ben"
+    marias_wins, bens_wins = 0, 0
+    #primes with a limit
+    n = max(nums)
+    primes = [True for _ in range(1, n + 1, 1)]
+    primes[0] = False
+    for a, is_prime in enumerate(primes, 1):
+        if a == 1 or not is_prime:
+            continue
+        for j in range(a + a, n + 1, a):
+            primes[j - 1] = False
+    # number of primes less than  nums
+    for _, n in zip(range(v), nums):
+        primes_count = len(list(filter(lambda v: v, primes[0: n])))
+        bens_wins += primes_count % 2 == 0
+        marias_wins += primes_count % 2 == 1
+    if marias_wins == bens_wins:
+        return None
+    return 'Maria' if marias_wins > bens_wins else 'Ben'
